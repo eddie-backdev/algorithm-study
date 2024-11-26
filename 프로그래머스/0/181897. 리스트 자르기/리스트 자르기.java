b54d15d03ac05a86c570f4b2cdbd3369dc901d98
HeeYong Lee <50799519+Dev-LHY@users.noreply.github.com>
2024-11-26 20:30:45 +0900

    [level 0] Title: 리스트 자르기, Time: 8.74 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        if (n == 1) {
+            for (int i = 0; i <= slicer[1]; i++) {
+                answer.add(num_list[i]);
+            }
+        }
+        if (n == 2) {
+            for (int i = slicer[0]; i < num_list.length; i++) {
+                answer.add(num_list[i]);
+            }
+        }
+        if (n == 3) {
+            for (int i = slicer[0]; i <= slicer[1]; i++) {
+                answer.add(num_list[i]);
+            }
+        }
+        if (n == 4) {
+            for (int i = slicer[0]; i <= slicer[1]; i+=slicer[2]) {
+                answer.add(num_list[i]);
+            }
+        }
+        
+        return answer.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+    }
+}
