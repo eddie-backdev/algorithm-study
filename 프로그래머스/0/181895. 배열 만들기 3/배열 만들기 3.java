21ea3bbfa09fa359ee02ad27f8fadae3247ac66d
HeeYong Lee <50799519+Dev-LHY@users.noreply.github.com>
2024-11-26 20:30:58 +0900

    [level 0] Title: 배열 만들기 3, Time: 13.33 ms, Memory: 97.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 0; i < intervals.length; i++) {
+            for ( int j = intervals[i][0]; j <= intervals[i][1]; j++) {
+                answer.add(arr[j]);
+            }
+        }
+        return answer.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+    }
+}
