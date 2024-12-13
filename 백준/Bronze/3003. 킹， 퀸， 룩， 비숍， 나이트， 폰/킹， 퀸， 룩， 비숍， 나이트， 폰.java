9d7c519bab56a1c051487cdcb6659abcbd826479
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2024-12-13 21:21:23 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 188 ms, Memory: 18340 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(1-scan.nextInt()+" ");
+        System.out.print(1-scan.nextInt()+" ");
+        System.out.print(2-scan.nextInt()+" ");
+        System.out.print(2-scan.nextInt()+" ");
+        System.out.print(2-scan.nextInt()+" ");
+        System.out.print(8-scan.nextInt()+" ");
+        scan.close();
+    }
+}
