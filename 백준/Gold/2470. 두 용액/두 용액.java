92ced899d921f7d38e3fe0c08604175f1c4de38d
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-03-09 13:36:06 +0900

    [Gold V] Title: 두 용액, Time: 332 ms, Memory: 28584 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int size = Integer.parseInt(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end = size-1;
+        int answerLeft = 0, answerRight = 0;
+        
+        int[] arr = new int[size];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        while (end > start) {
+            int sum = arr[end] + arr[start];
+            if (min > Math.abs(sum)) {
+                min = Math.abs(sum);
+                answerLeft = arr[start];
+                answerRight = arr[end];
+
+                if (sum == 0) {
+                    break;
+                }
+            }
+            if (sum < 0) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        System.out.println(answerLeft + " " + answerRight);
+    }
+}
