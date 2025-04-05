6b6569151147036489c8de6c49b11898cc56ca8f
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-04-05 21:15:29 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int target = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < 7; i++) {
+            if ((target & (1<<i)) != 0) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
