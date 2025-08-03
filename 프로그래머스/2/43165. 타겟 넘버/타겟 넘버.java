05b771e74f6855013952b601c0059d79ac508c29
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-08-04 00:02:44 +0900

    [level 2] Title: 타겟 넘버, Time: 9.37 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -4,24 +4,26 @@ class Solution {
     int answer;
     
     public int solution(int[] numbers, int target) {
-        answer = 0;
+        this.answer = 0;
         this.numbers = numbers;
         this.target = target;
         
-        dfs(0, 0);
+        dfs(0,0);
         
         return answer;
     }
     
-    public void dfs(int idx, int sum) {
-        if (idx == numbers.length) {
-            if (sum == target) {
+    void dfs(int index, int sum) {
+        // 탈출 조건
+        if (index == numbers.length) {
+            if(sum == target) {
                 answer++;
             }
             return;
         }
         
-        dfs(idx + 1, sum + numbers[idx]);
-        dfs(idx + 1, sum - numbers[idx]);
+        // 수행동작
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
     }
 }
