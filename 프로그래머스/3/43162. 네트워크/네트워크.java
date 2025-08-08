7c12b6fa0596ea9281e0683bd335f4402e978dec
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-08-08 22:08:18 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,35 +1,25 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
-    static boolean[] visited;
-    static int netCnt;
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
+        int answer = 0;
+        
+        boolean[] visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                bfs(i, computers);
-                netCnt++;
+            if (visited[i] == false) {
+                answer++;
+                dfs(visited, computers, i);
             }
         }
-        return netCnt;
+        return answer;
     }
     
-    void bfs(int start ,int[][] coms) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        visited[start] = true;
+    public void dfs(boolean[] visited, int[][] computers, int node) {
+        visited[node] = true;
         
-        while (!q.isEmpty()) {
-            int v = q.poll();
-            for (int i = 0; i < coms.length; i++) {
-                if (!visited[i] && coms[v][i] == 1) {
-                    q.offer(i);
-                    visited[i] = true;
-                }
-            }
+        for (int i = 0; i < computers.length; i++) {
+           if (visited[i] == false && computers[node][i] == 1) {
+               dfs(visited, computers, i);
+           }
         }
     }
-
 }
