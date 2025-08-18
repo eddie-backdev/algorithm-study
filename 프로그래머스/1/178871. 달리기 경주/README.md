e7fdd04b9b1df802e61741334c8913441432e417
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-08-18 22:53:45 +0900

    [level 1] Title: 달리기 경주, Time: 267.90 ms, Memory: 375 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=java) 
 
 ### 성능 요약
 
-메모리: 402 MB, 시간: 759.22 ms
+메모리: 375 MB, 시간: 267.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 10일 22:56:41
+2025년 08월 18일 22:53:41
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,34 +2,23 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-		String[] answer = new String[players.length];
+        Map<String, Integer> playerMap = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            playerMap.put(players[i], i);
+        }
+        
+        for (String callingPlayer : callings) {
+            int currentRank = playerMap.get(callingPlayer);
 
-		Map<String, Integer> playerMap = new HashMap<>();
-		Map<Integer, String> rankMap = new HashMap<>();
+            String frontPlayer = players[currentRank - 1];
 
-		for (int i = 0; i < players.length; i++) {
-			playerMap.put(players[i], i);
-			rankMap.put(i, players[i]);
-		}
+            players[currentRank - 1] = callingPlayer;
+            players[currentRank] = frontPlayer;
 
-		for (int i = 0; i < callings.length; i++) {
-
-			int currentRank = playerMap.get(callings[i]);
-			String player = rankMap.get(currentRank);
-
-			String frontPlayer = rankMap.get(currentRank - 1);
-
-			playerMap.put(player, currentRank - 1);
-			playerMap.put(frontPlayer, currentRank);
-
-			rankMap.put(currentRank - 1, player);
-			rankMap.put(currentRank, frontPlayer);
-		}
-
-		for (int i = 0; i < players.length; i++) {
-			answer[i] = rankMap.get(i);
-		}
-
-		return answer;
-	}
+            playerMap.put(callingPlayer, currentRank - 1);
+            playerMap.put(frontPlayer, currentRank);
+        }
+        
+        return players;
+    }
 }
