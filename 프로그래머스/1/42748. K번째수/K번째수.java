bcc51fd5b7f5603731946d5d8d82bff97be00c73
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-08-22 15:17:09 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,15 +1,18 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
         for (int i = 0; i < commands.length; i++) {
-            int[] splitArr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(splitArr);
-            answer[i] = splitArr[commands[i][2]-1];
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int target = commands[i][2];
+            
+            int[] temp = Arrays.copyOfRange(array, start - 1, end);
+            Arrays.sort(temp);
+            answer[i] = temp[target - 1];
         }
-        
         return answer;
     }
 }
