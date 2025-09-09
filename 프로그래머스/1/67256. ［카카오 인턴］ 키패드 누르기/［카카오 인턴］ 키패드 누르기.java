beb0fe4b14c2df7d9dea4a544289ce2002a097c2
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-09-09 23:11:07 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 4.96 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+
+        int left = 10;
+        int right = 12;
+
+        for (int i : numbers) {
+            if (i == 1 || i == 4 || i == 7) {
+                answer += "L";
+                left = i;
+            } else if (i == 3 || i == 6 || i == 9) {
+                answer += "R";
+                right = i;
+            } else {
+                if (i == 0) i = 11;
+
+                int leftDistance = Math.abs(i - left) / 3 + Math.abs(i - left) % 3;
+                int rightDistance = Math.abs(i - right) / 3 + Math.abs(i - right) % 3;
+
+                if (leftDistance < rightDistance) {
+                    answer += "L";
+                    left = i;
+                } else if (leftDistance > rightDistance) {
+                    answer += "R";
+                    right = i;
+                } else {
+                    if (hand.equals("left")) {
+                        answer += "L";
+                        left = i;
+                    } else {
+                        answer += "R";
+                        right = i;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
