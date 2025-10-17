9708221e050a3e7a17b1e6714277beeed3af4e12
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-10-17 22:20:07 +0900

    [level 2] Title: 타겟 넘버, Time: 216.63 ms, Memory: 163 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     private int[] numbers;
     private int target;
@@ -7,8 +9,32 @@ class Solution {
         this.numbers = numbers;
         this.target = target;
         this.answer = 0;
-        dfs(0, 0);
+        // dfs(0, 0);
+        bfs();
         return answer;
+    }
+    
+    // bfs로도 풀어보기
+    private void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
+        int length = numbers.length;
+        
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            int sum = now[0];
+            int idx = now[1];
+            
+            if (idx == length) {
+                if (sum == target) {
+                    answer++;
+                }
+                continue;
+            }
+            
+            q.offer(new int[]{sum + numbers[idx], idx + 1});
+            q.offer(new int[]{sum - numbers[idx], idx + 1});
+        }
     }
 
     private void dfs(int sum, int idx) {
