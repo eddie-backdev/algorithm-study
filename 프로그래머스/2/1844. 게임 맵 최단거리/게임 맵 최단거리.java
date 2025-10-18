b9705b2358a3d85c44abe423d2ab10faae95128c
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-10-18 21:06:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.54 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,39 +4,32 @@ class Solution {
     
     int[] dx = {0, 1, -1, 0};
     int[] dy = {1, 0, 0, -1};
+    int[][] visited;
     
     public int solution(int[][] maps) {
-        int[][] visited = new int[maps.length][maps[0].length];
-        bfs(maps, visited);
-        int answer = visited[maps.length - 1][maps[0].length - 1];
-        if (answer == 0) {
-            return -1;
-        }
+        visited = new int[maps.length][maps[0].length];
+        bfs(maps);
+        int answer = visited[maps.length - 1][maps[0].length - 1] > 0 ? visited[maps.length - 1][maps[0].length - 1] : -1;
         return answer;
     }
     
-    void bfs(int[][] maps, int[][] visited) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0});
+    void bfs(int[][] maps) {
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offer(new int[]{0,0});
         visited[0][0] = 1;
         
-        while(!q.isEmpty()) {
-            int[] current = q.poll();
+        while (!dq.isEmpty()) {
+            int[] current = dq.poll();
             int x = current[0];
             int y = current[1];
             
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if (nx < 0 || nx > maps.length - 1 || ny < 0 || ny > maps[0].length - 1) {
-                    continue;
-                }
-                
-                if (visited[nx][ny] == 0 && maps[nx][ny] == 1) {
-                    visited[nx][ny] = visited[x][y] + 1;
-                    q.add(new int[]{nx, ny});
-                }
+                int nx = dx[i] + x;
+                int ny = dy[i] + y;
+                if (nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length || maps[nx][ny] == 0) continue;
+                if (visited[nx][ny] > 0) continue;
+                visited[nx][ny] = visited[x][y] + 1;
+                dq.offer(new int[]{nx, ny});
             }
         }
     }
