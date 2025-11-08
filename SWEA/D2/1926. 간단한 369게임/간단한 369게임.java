c70e9e4f139effedbd7983361e3e687c9c50f4c7
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-11-09 02:10:57 +0900

    [D2] Title: 간단한 369게임, Time: 84 ms, Memory: 22,528 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            String num = String.valueOf(i);
+            int cnt = 0;
+
+            for (int j = 0; j < num.length(); j++) {
+                char c = num.charAt(j);
+                if (c == '3' || c == '6' || c == '9') {
+                    cnt++;
+                }
+            }
+
+            if (cnt != 0) {
+                for (int j = 0; j < cnt; j++) {
+                    sb.append('-');
+                }
+            } else {
+                sb.append(i);
+            }
+            sb.append(' ');
+        }
+        System.out.print(sb);
+    }
+}
