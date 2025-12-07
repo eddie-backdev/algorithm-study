f28ad7a394582ee3c052af40f82261cfc6f5333d
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-12-07 23:12:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.88 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,35 +2,44 @@ import java.util.*;
 
 class Solution {
     
-    int[] dx = {0, 1, -1, 0};
-    int[] dy = {1, 0, 0, -1};
-    int[][] visited;
-    
+    int[] dx = {0, 0, -1, 1};
+    int[] dy = {-1, 1, 0, 0};
+    boolean[][] visited;
+    int[][] score;
+
     public int solution(int[][] maps) {
-        visited = new int[maps.length][maps[0].length];
+        visited = new boolean[maps.length][maps[0].length];
+        score = new int[maps.length][maps[0].length];
+        score[0][0] = 1;
         bfs(maps);
-        int answer = visited[maps.length - 1][maps[0].length - 1] > 0 ? visited[maps.length - 1][maps[0].length - 1] : -1;
-        return answer;
-    }
-    
-    void bfs(int[][] maps) {
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.offer(new int[]{0,0});
-        visited[0][0] = 1;
-        
-        while (!dq.isEmpty()) {
-            int[] current = dq.poll();
-            int x = current[0];
-            int y = current[1];
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + x;
-                int ny = dy[i] + y;
-                if (nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length || maps[nx][ny] == 0) continue;
-                if (visited[nx][ny] > 0) continue;
-                visited[nx][ny] = visited[x][y] + 1;
-                dq.offer(new int[]{nx, ny});
-            }
+        if (score[maps.length - 1][maps[0].length - 1] == 0) {
+            return -1;
+        } else {
+            return score[maps.length - 1][maps[0].length - 1];
         }
     }
+
+    void bfs(int[][] maps) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{0, 0});
+        visited[0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
+
+                if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length) {
+                    if (!visited[nx][ny] && maps[nx][ny] == 1) {
+                        visited[nx][ny] = true;
+                        score[nx][ny] = score[current[0]][current[1]] + 1;
+                        q.offer(new int[]{nx, ny});
+                    }
+                }
+            }
+
+        }
+    }
+    
 }
