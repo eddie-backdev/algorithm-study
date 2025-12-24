a4c93f02e2b4eb882bf1a6cb046faeaabd734e70
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2025-12-25 02:45:34 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 42.10 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        
+        while (true) {
+            boolean changed = false;
+            
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= 50 && arr[i] % 2 == 0) {
+                    arr[i] /= 2;
+                    changed = true;
+                } else if (arr[i] < 50 && arr[i] % 2 != 0) {
+                    arr[i] = arr[i] * 2 + 1;
+                    changed = true;
+                }
+            }
+            
+            if (!changed) {
+                break;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
