e9ce1e04290d5652ab90aa6079ae1b1862b77669
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-01-05 17:51:25 +0900

    [level 0] Title: 가까운 수, Time: 1.09 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        Arrays.sort(array);
+        
+        int answer = array[0];
+
+        int min = Math.abs(n - array[0]); 
+        
+        for (int i = 1; i < array.length; i++) {
+            int temp = Math.abs(n - array[i]);
+
+            if (temp < min) {
+                min = temp;
+                answer = array[i];
+            }
+        }
+        
+        return answer;
+    }
+}
