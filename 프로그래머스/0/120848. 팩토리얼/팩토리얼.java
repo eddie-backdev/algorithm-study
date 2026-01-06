c55baa388ec539239561b4f9740aefd0f7e2660b
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-01-06 13:39:36 +0900

    [level 0] Title: 팩토리얼, Time: 3.29 ms, Memory: 90.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n + 1];
+        
+        arr[0] = 1;
+        arr[1] = 1;
+        
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i - 1] * i;
+        }
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] <= n) {
+                answer = i;
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
