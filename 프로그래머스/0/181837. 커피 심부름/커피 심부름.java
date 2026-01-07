3f067f92945f0f502fa1b7cd9a6d74c75e3bdcc2
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-01-07 22:30:32 +0900

    [level 0] Title: 커피 심부름, Time: 0.80 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -3,9 +3,7 @@ class Solution {
         int answer = 0;
         
         for (String o : order) {
-            if (o.contains("americano")) {
-                answer += 4500;
-            } else if (o.contains("cafelatte")) {
+            if (o.contains("cafelatte")) {
                 answer += 5000;
             } else {
                 answer += 4500;
