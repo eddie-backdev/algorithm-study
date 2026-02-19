a404a7bc5fd775b943bb505804d36c7b8dfd737c
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-02-20 01:46:18 +0900

    [level 2] Title: 기능개발, Time: 2.88 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,38 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> q = new LinkedList<>();
-        ArrayList<Integer> list = new ArrayList<>();
-
+        List<Integer> answer = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        
         for (int i = 0; i < progresses.length; i++) {
-            if ((100 - progresses[i]) % speeds[i] == 0) {
-                q.add((100 - progresses[i]) / speeds[i]);
-            } else {
-                q.add((100 - progresses[i]) / speeds[i] + 1);
-            }
+            int work = 100 - progresses[i];
+            
+            int deploy = work % speeds[i] == 0 ? work / speeds[i] : work / speeds[i] + 1;
+            
+            dq.add(deploy);
         }
         
-        int num = q.poll();
-        int count = 1;
-        
-        while (!q.isEmpty()) {
-            if (num >= q.peek()) {
+        while (!dq.isEmpty()) {
+            int currentDeploy = dq.poll();
+            int count = 1;
+            
+            while (!dq.isEmpty() && currentDeploy >= dq.peek()) {
                 count++;
-                q.poll();
-            } else {
-                list.add(count);
-                count = 1;
-                num = q.poll();
+                dq.poll();
             }
-        }
-        list.add(count);
-
-        int[] answer = new int[list.size()];
-
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
+            
+            answer.add(count);
         }
         
-        return answer;
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
