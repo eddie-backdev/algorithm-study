81941718bf37626d8a5c4f10bf829c9992a3dda0
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-02-22 20:54:14 +0900

    [level 1] Title: 신고 결과 받기, Time: 172.63 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,51 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int n = id_list.length;
-
-        Map<String, Integer> idx = new HashMap<>();
-        Set<String>[] reportedList = new HashSet[n];
-        int[] reportedCount = new int[n];
+        int[] answer = new int[id_list.length];
+        Map<String, HashSet<String>> reportedUser = new HashMap<>();
+        Map<String, Integer> reportCnt = new HashMap<>();
         
-        for (int i = 0; i < n; i++) {
-            idx.put(id_list[i], i);
+        // map 초기화
+        for (String id : id_list) {
+            reportedUser.put(id, new HashSet<>());
+            reportCnt.put(id, 0);
         }
-
-
-        for (int i = 0; i < n; i++) {
-            reportedList[i] = new HashSet<>();
-        }
-
-        // 1. 중복 신고 제거 + 신고 누적
+        
+        // 신고된 유저 reportedUser Set에 추가
         for (String r : report) {
-            String[] sp = r.split(" ");
-            String from = sp[0];
-            String to = sp[1];
-
-            int fromIdx = idx.get(from);
-            int toIdx = idx.get(to);
-
-            if (reportedList[fromIdx].add(to)) {
-                reportedCount[toIdx]++;
-            }
+            String[] users = r.split(" ");
+            reportedUser.get(users[1]).add(users[0]);
         }
-
-        // 2. 정지된 유저
-        boolean[] suspended = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            if (reportedCount[i] >= k) suspended[i] = true;
-        }
-
-        // 3. 결과 메일 수 계산
-        int[] answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            for (String t : reportedList[i]) {
-                if (suspended[idx.get(t)]) {
-                    answer[i]++;
+        
+        // 신고된 유저 Set 사이즈가 k 이상이면 Cnt map에 몇 번 신고됐는지 추가
+        for (String id : id_list) {
+            Set<String> reportedSet = reportedUser.get(id);
+            if (reportedSet.size() >= k) {
+                for (String user : reportedSet) {
+                    reportCnt.put(user, reportCnt.get(user) + 1);
                 }
             }
         }
-
+        
+        // 정지된 유저가 있으면 신고한 유저에게 결과 반환
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = reportCnt.get(id_list[i]);
+        }
+        
         return answer;
     }
 }
