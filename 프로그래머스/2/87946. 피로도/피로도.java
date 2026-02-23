e216c3a05cccf08ceddfff70b990e39bcb7b8f37
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-02-23 23:27:45 +0900

    [level 2] Title: 피로도, Time: 4.48 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,19 +1,23 @@
 class Solution {
-    static boolean[] visited;
-    static int maxDG;
+    
+    int answer = 0;
+    boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        maxDG(k, dungeons, 0);
-        return maxDG;
+        
+        dfs(k, dungeons, 0);
+        
+        return answer;
     }
     
-    public void maxDG(int k, int[][] dungeons, int count) {
-        maxDG = Math.max(maxDG, count);
+    void dfs(int k, int[][] dungeons, int count) {
+        answer = Math.max(answer, count);
+        
         for (int i = 0; i < dungeons.length; i++) {
             if (!visited[i] && k >= dungeons[i][0]) {
                 visited[i] = true;
-                maxDG(k - dungeons[i][1], dungeons, count + 1);
+                dfs(k - dungeons[i][1], dungeons, count + 1);
                 visited[i] = false;
             }
         }
