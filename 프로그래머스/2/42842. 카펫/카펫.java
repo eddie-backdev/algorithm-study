bd2627a24e6ad744dbdf52fc932a467a3eb01213
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-02-23 16:39:26 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int total = brown + yellow;
+        int sum = brown + yellow;
         
-        for (int i = 3; i <= total / i; i++) {
-            if (total % i == 0) {
-                int width = total / i;
-                int yWidth = width - 2;
-                int yHeight = i - 2;
+        for (int i = 3; i <= sum / 2; i++) {
+            if (sum % i == 0) {
+                int w = sum / i;
                 
-                if (yWidth * yHeight == yellow) {
-                    answer[0] = width;
+                if ((i - 2) * (w - 2) == yellow) {
+                    answer[0] = w;
                     answer[1] = i;
+                    break;
                 }
             }
-            
         }
         return answer;
     }
