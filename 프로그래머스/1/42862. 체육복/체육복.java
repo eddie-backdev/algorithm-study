53d6f4ec1c698182df4a8ad242bf28d0b38ee716
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-02-24 14:40:27 +0900

    [level 1] Title: 체육복, Time: 1.06 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
@@ -16,18 +16,25 @@ class Solution {
                     break;
                 }
             }
-        } 
+        }
         
         for (int i = 0; i < lost.length; i++) {
+            if (lost[i] == -1) {
+                continue;
+            }
             for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+                if (reserve[j] == -1) {
+                    continue;
+                }
+                
+                if (reserve[j] == lost[i] - 1 || reserve[j] == lost[i] + 1) {
                     answer++;
                     reserve[j] = -1;
                     break;
                 }
             }
-        } 
-    
+        }
+        
         return answer;
     }
 }
