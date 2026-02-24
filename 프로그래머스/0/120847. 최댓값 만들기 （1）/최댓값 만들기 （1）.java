572b13f2dc221197404e3d03fbeb183c6d1d6795
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-02-24 18:46:54 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.40 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,17 +1,10 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int max = 0;
-
-        for(int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] * numbers[j] > max) {
-                    max = numbers[i] * numbers[j];
-                }
-            }
-        }
-        answer = max;
-
-        return answer;
+        Arrays.sort(numbers);
+        int idx = numbers.length;
+        
+        return numbers[idx - 1] * numbers[idx - 2];
     }
 }
