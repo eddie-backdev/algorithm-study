4dac393e7112f3ed7a5698e55861791a5716834b
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-03-05 22:56:59 +0900

    [level 2] Title: H-Index, Time: 0.87 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,15 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        int n = citations.length;
+        
         Arrays.sort(citations);
         
-        for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i;
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            
             if (citations[i] >= h) {
                 answer = h;
                 break;
             }
         }
+        
         return answer;
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 1.53 ms
+메모리: 84.9 MB, 시간: 0.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 13일 22:58:02
+2026년 03월 05일 22:56:55
 
 ### 문제 설명
 
