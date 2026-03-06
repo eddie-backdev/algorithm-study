f7dfc15a008ceb25f35444c6c075ee4a58532cf8
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-03-06 21:11:08 +0900

    [level 2] Title: 가장 큰 수, Time: 158.26 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,13 @@
-import java.util.Arrays;
-import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
-    public String solution(int[] numbers) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        String[] strNumbers = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            strNumbers[i] = String.valueOf(numbers[i]);
-        }
-        Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
+    public String solution(int[] numbers) {
+        String answer = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((a, b) -> (b + a).compareTo(a + b))
+                .collect(Collectors.joining());
         
-        if (strNumbers[0].equals("0")) {
-            return "0";
-        }
-        
-        for (String strNumber : strNumbers) {
-            sb.append(strNumber);
-        }
-        return sb.toString();
+        return answer.startsWith("0") ? "0" : answer;
     }
 }
