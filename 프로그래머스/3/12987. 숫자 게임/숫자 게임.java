1ccc833ca88894f7b3fe48b12ddfe750432b6e06
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-03-11 22:45:23 +0900

    [level 3] Title: 숫자 게임, Time: 108.17 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int aIdx = 0;
+        int bIdx = 0;
+        
+        while (aIdx < A.length && bIdx < B.length) {
+            if (B[bIdx] > A[aIdx]) {
+                answer++;
+                aIdx++;
+                bIdx++;
+            } else {
+                bIdx++;
+            }
+        }
+        
+        return answer;
+    }
+}
