061d9ca70ee60c6c4c5ee9b9dc9b122e296f4fa0
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-04-15 17:52:19 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 6.38 ms, Memory: 133 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            long x = numbers[i];
+
+            if (x % 2 == 0) {
+                answer[i] = x + 1;
+            } else {
+                long lastIndex = ~x & (x + 1);
+                answer[i] = (x | lastIndex) & ~(lastIndex >> 1);
+            }
+        }
+
+        return answer;
+    }
+}
