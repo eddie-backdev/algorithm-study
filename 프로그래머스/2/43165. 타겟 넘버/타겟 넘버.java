7997a74f9a3d7136c0675d82370aa717e276da58
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-04-19 23:08:56 +0900

    [level 2] Title: 타겟 넘버, Time: 5.28 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,48 +1,25 @@
-import java.util.*;
-
 class Solution {
-    private int[] numbers;
-    private int target;
-    private int answer;
-
+    int[] numbers;
+    int answer;
+    
     public int solution(int[] numbers, int target) {
         this.numbers = numbers;
-        this.target = target;
-        this.answer = 0;
-        // dfs(0, 0);
-        bfs();
+        dfs(target, 0, 0);
         return answer;
     }
     
-    // bfs로도 풀어보기
-    private void bfs() {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
-        int length = numbers.length;
-        
-        while(!q.isEmpty()) {
-            int[] now = q.poll();
-            int sum = now[0];
-            int idx = now[1];
-            
-            if (idx == length) {
-                if (sum == target) {
-                    answer++;
-                }
-                continue;
-            }
-            
-            q.offer(new int[]{sum + numbers[idx], idx + 1});
-            q.offer(new int[]{sum - numbers[idx], idx + 1});
-        }
-    }
-
-    private void dfs(int sum, int idx) {
+    void dfs(int target, int current, int idx) {
         if (idx == numbers.length) {
-            if (sum == target) answer++;
+            if (target == current) {
+                answer++;
+            }
             return;
         }
-        dfs(sum + numbers[idx], idx + 1);
-        dfs(sum - numbers[idx], idx + 1);
+        
+        int p = current + numbers[idx];
+        int m = current - numbers[idx];
+        
+        dfs(target, p, idx + 1);
+        dfs(target, m, idx + 1);
     }
 }
