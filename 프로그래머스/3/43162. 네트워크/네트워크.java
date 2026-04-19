a7255f23dfd90181db24a76d038e13070b9de3ae
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-04-20 01:36:31 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,30 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        
         boolean[] visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                bfs(computers, visited, i);
+                dq.offer(i);
+                
+                while(!dq.isEmpty()) {
+                    int node = dq.poll();
+                    visited[node] = true;
+                    for (int j = 0; j < n; j++) {
+                        if (!visited[j] && computers[node][j] == 1) {
+                            visited[j] = true;
+                            dq.offer(j);
+                        }
+                    }
+                }
+                
                 answer++;
-            }
+            }   
         }
         return answer;
-    }
-    
-    void bfs(int[][] computers, boolean[] visited, int v) {
-        Deque<Integer> dq = new ArrayDeque<>();
-        dq.offer(v);
-        visited[v] = true;
-        
-        while (!dq.isEmpty()) {
-            Integer w = dq.poll();
-            for (int i = 0; i < computers.length; i++) {
-                if (computers[w][i] != 0  && !visited[i]) {
-                    visited[i] = true;
-                    dq.offer(i);
-                }
-            }
-        }
     }
 }
