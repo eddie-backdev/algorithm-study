7e324bcf59715432ff1be19c8b59bcae3b11d582
Eddie Lee <50799519+eddie-backdev@users.noreply.github.com>
2026-06-13 22:42:44 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,37 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
+        int[] clothes = new int[n + 2];
         
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    answer++;
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    break;
-                }
-            }
+        for (int r : reserve) {
+            clothes[r]++;
         }
         
-        for (int i = 0; i < lost.length; i++) {
-            if (lost[i] == -1) {
-                continue;
+        for (int l : lost) {
+            clothes[l]--;
+        }
+        
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (clothes[i] == -1) {
+                if (clothes[i - 1] == 1) {
+                    clothes[i - 1]--;
+                    clothes[i]++;
+                } else if (clothes[i + 1] == 1) {
+                    clothes[i + 1]--;
+                    clothes[i]++;
+                }
             }
-            for (int j = 0; j < reserve.length; j++) {
-                if (reserve[j] == -1) {
-                    continue;
-                }
-                
-                if (reserve[j] == lost[i] - 1 || reserve[j] == lost[i] + 1) {
-                    answer++;
-                    reserve[j] = -1;
-                    break;
-                }
+            
+            if (clothes[i] >= 0) {
+                answer++;
             }
         }
         
